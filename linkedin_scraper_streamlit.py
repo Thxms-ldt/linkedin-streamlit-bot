11752cc4b29747c9ab41bf3fe793f04edd481471
Thxms-ldt <thomas.laudet@wefiit.com>
2025-06-12 10:43:11 +0200

    Update linkedin_scraper_streamlit.py

--- linkedin_scraper_streamlit.py
+++ linkedin_scraper_streamlit.py
@@ -12,8 +12,21 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
+CHROME_BIN_PATH = "/usr/bin/chromium-browser"
+CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+options.binary_location = CHROME_BIN_PATH
+service = Service(executable_path=CHROMEDRIVER_PATH)
+
+driver = webdriver.Chrome(service=service, options=options)
 # Charger les identifiants LinkedIn
 load_dotenv()
 EMAIL = os.getenv("LINKEDIN_EMAIL") or "TON_EMAIL_LINKEDIN"
